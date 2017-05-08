2bff8c8e3511c3118f01e952d5d278f0be5898bc
zhangxiao <zhangxiao@yixue2015.com>
2017-05-08 17:43:06 +0800

    --test

--- xyWeather/src/main/java/com/xy/weather/activity/WeatherActivity.java
+++ xyWeather/src/main/java/com/xy/weather/activity/WeatherActivity.java
@@ -55,6 +55,7 @@ public class WeatherActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message msg) {
             if (msg.what == 1) {
+                Log.d(TAG, msg.obj.toString());
                 try {
                     JSONObject result = new JSONObject(msg.obj.toString());
                     JSONObject today = result.getJSONObject("retData").getJSONObject("today");
